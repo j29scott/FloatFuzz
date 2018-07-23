62b02048e1dad631cf6e19c499001e5f51804fb2
Joseph Scott <joescott@gra-login2.graham.sharcnet>
2018-07-23 00:49:57 -0400

    prelim

---
+++ test/lib/python3.5/abc.py
@@ -0,0 +1,1 @@
+/cvmfs/soft.computecanada.ca/nix/store/v29pphgl66qjvjck1mn4pcm5g9agk5kh-python3-3.5.2/lib/python3.5/abc.py
---
+++ test/lib/python3.5/sitecustomize.py
@@ -0,0 +1,1 @@
+import sys; sys.path.append('/cvmfs/soft.computecanada.ca/nix/store/v29pphgl66qjvjck1mn4pcm5g9agk5kh-python3-3.5.2/lib/python3.5/site-packages')
